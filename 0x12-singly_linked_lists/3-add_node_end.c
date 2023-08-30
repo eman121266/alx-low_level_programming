d2e3a968278c6250fa201e5bd99a4d60206a7a62
Eman <dawoodeman71@gmail.com>
2023-08-30 08:03:49 -0700

    k

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		while (node->next)
 			node = node->next;
-		node->next = new node;
+		node->next = new_node;
 	}
 	else
 		*head = new_node;
